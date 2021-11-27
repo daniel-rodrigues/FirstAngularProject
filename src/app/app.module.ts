895a95bfd43458849d3133e3562862de3445f7e2
Daniel Rodrigues <inf.daniel@gmail.com>
2021-11-27 17:34:45 -0300

    adding routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navigation/menu/menu.component';
 import { HomeComponent } from './navigation/home/home.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { AboutComponent } from './institutional/about/about.component';
+import { ContactComponent } from './institutional/contact/contact.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
-  providers: [],
+  providers: [
+  {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
